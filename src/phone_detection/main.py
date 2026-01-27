f09cb991841e1503f5fcbc13eab2d3964e148962
SKoonkor <suttikoon.koonkor@outlook.com>
2026-01-27 16:51:36 +0700

    Added face detection

--- src/phone_detection/main.py
+++ src/phone_detection/main.py
@@ -1,5 +1,7 @@
 # Python libraries
+
 import cv2
+import mediapipe as mp
 
 def main():
     """
@@ -15,7 +17,8 @@ def main():
 
 
     # Open default camera (usually index 0)
-    cap = cv2.VideoCapture(0)  # Change the index for different camera if there is any, later tho
+    cap = cv2.VideoCapture(0)
+        # Change the index for different camera if there is any, later tho
 
     # Check if the camera opened successfully 
     if not cap.isOpened():
@@ -24,21 +27,70 @@ def main():
     print ("Camera opened successfully. Press 'q' to quite.")
 
 
-    while True:
-        # Read a frame from the webcam
-        ret, frame = cap.read()
 
-        if not ret:
-            print ("WARNING: Failed to grab frame.")
-            break
+    # MediaPipe setup
+    mp_face = mp.solutions.face_detection
+    mp_draw = mp.solutions.drawing_utils
+
+    # Model_selection:
+    # 0 = short-range (~ within 2m, for laptop used)
+    # 1 = full-range (works better for far distances)
+
+    with mp_face.FaceDetection(model_selection=0, min_detection_confidence=0.6) as face_detector:
+        print ("Running face detection")
 
 
-        # Show the frame
-        cv2.imshow("Phone Detection", frame)
+        while True:
+            # Read a frame from the webcam
+            ret, frame = cap.read()
 
-        # Exit when 'q' is press
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+            if not ret:
+                print ("WARNING: Failed to grab frame.")
+                break
+
+            # MediaPipe expects RGB
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Run face detection
+            results = face_detector.process(frame_rgb)
+
+            # Draw Detections
+            if results.detections:
+                for det in results.detections:
+                    # Draw bounding box + keypoints (eyes, nose, mouth) using MediaPipe helper
+                    mp_draw.draw_detection(frame, det)
+
+                    # Show how confidence for the detections
+                    score = det.score[0] if det.score else 0.0
+                    cv2.putText(
+                            frame,
+                            f"Face: {score:.2f}",
+                            (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1.0,
+                            (0, 255, 0),
+                            2,
+                            cv2.LINE_AA,
+                            )
+                else:
+                    cv2.putText(
+                            frame,
+                            "NO FACE",
+                            (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            1.0,
+                            (0, 0, 255),
+                            2,
+                            cv2.LINE_AA,
+                            )
+
+
+            # Show the frame
+            cv2.imshow("Phone Detection", frame)
+
+            # Exit when 'q' is press
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
 
     # Cleanup
@@ -48,5 +100,5 @@ def main():
 
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
